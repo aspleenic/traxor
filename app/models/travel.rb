016a2fe6de41052e6eeeb56a67717a16c105489f
aspleenic <pjhagerty@gmail.com>
2017-02-25 12:20:59 -0500

    Adding info in the README, adjusting some of the math involved

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,7 +1,3 @@
 class Travel < ApplicationRecord
 
-  # def total_travel_cost
-  #   @travel.total_travel_cost = @travel.hotel_cost_total + @travel.airfare_cost_total + @travel.other_expenses_total
-  # end
-
 end
